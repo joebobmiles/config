717cff5206d20e7566d8c48625b4ec1b2609c2c4
Joseph R Miles <joe@jrm.dev>
2020-12-31 07:26:28 -0800

    feat: added typescript specific rules

--- configs/eslint-typescript/index.js
+++ configs/eslint-typescript/index.js
@@ -16,6 +16,104 @@ module.exports = {
         "allowDeclarations": false,
         "allowDefinitionFiles": true
       }
+    ],
+    /* Plugin rules */
+    /* Core rules */
+    "indent": "off",
+    "@typescript-eslint/indent": [ 
+      `error`,
+      2,
+      {
+        "offsetTernaryExpressions": true,
+      }
+    ],
+    "semi": "off",
+    "@typescript-eslint/semi": [
+      "error",
+      "always",
+      {
+        "omitLastInOneLineBlock": true,
+      }
+    ],
+    "brace-style": "off",
+    "@typescript-eslint/brace-style": [
+      "error",
+      "allman",
+      {
+        "allowSingleLine": true
+      }
+    ],
+    /* Quote rules */
+    "quotes": "off",
+    "@typescript-eslint/quotes": [
+      "error",
+      "double",
+      {
+        "allowTemplateLiterals": true
+      }
+    ],
+    /* Spacing rules */
+    "comma-spacing": "off",
+    "@typescript-eslint/comma-spacing": [
+      "error",
+      {
+        "before": false,
+        "after": true
+      }
+    ],
+    "keyword-spacing": "off",
+    "@typescript-eslint/keyword-spacing": [
+      "error",
+      {
+        "before": true,
+        "after": true,
+      }
+    ],
+    /* Paren/bracket/brace rules */
+    "no-extra-parens": "off",
+    "@typescript-eslint/no-extra-parens": [
+      "error",
+      "all",
+      {
+        "conditionalAssign": false,
+        "returnAssign": false,
+        "nestedBinaryExpressions": false,
+        "ignoreJSX": `all`,
+        "enforceForArrowConditionals": false,
+        "enforceForSequenceExpressions": false,
+        "enforceForNewInMemberExpressions": false,
+      }
+    ],
+    /* Variable declaration rules */
+    "init-declarations": "off",
+    "@typescript-eslint/init-declarations": [
+      "error",
+      "always"
+    ],
+    /* Notation rules */
+    "dot-notation": "off",
+    "@typescript-eslint/dot-notation": [
+      "error",
+      {
+        "allowKeywords": true
+      }
+    ],
+    /* Function rules */
+    "default-param-last": "off",
+    "@typescript-eslint/default-param-last": "error",
+    /* Miscellaneous rules */
+    "no-invalid-this": "off",
+    "@typescript-eslint/no-invalid-this": "error",
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": [
+      "error",
+      {
+        "arrays": `never`,
+        "objects": `always`,
+        "imports": `always`,
+        "exports": `always`,
+        "functions": `never`
+      }
     ]
   },
 };
