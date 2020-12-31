db91862d4d9b7bb9808e6d01517cf74a03ad3ff5
Joseph R Miles <joe@jrm.dev>
2020-12-31 07:33:33 -0800

    fix(eslint-config-typescript): added parserOptions

--- configs/eslint-typescript/index.js
+++ configs/eslint-typescript/index.js
@@ -1,5 +1,11 @@
 module.exports = {
   "parser": `@typescript-eslint/parser`,
+  "parserOptions": {
+    "tsconfigRootDir": __dirname,
+    "project": [
+      "./tsconfig.json"
+    ]
+  },
   "plugins": [
     `@typescript-eslint`
   ],
