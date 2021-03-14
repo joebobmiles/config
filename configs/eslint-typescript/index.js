b0223bea28959d9045009fc681bca3521d5d2ffd
Joseph R Miles <joe@jrm.dev>
2021-03-13 16:09:45 -0800

    feat: reconfigured no-unused-vars for typescript

--- configs/eslint-typescript/index.js
+++ configs/eslint-typescript/index.js
@@ -15,6 +15,8 @@ module.exports = {
     "plugin:@typescript-eslint/recommended"
   ],
   "rules": {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [ "error" ],
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-namespace": [
       "error",
